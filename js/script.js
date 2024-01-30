4242b47b40b65190b0386129a8d1e6f8e7da66ea
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2024-01-30 23:26:08 +0600

    banner section completed with mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function mobileMenuToggle() {
     let nav = document.querySelector('.navbar-link');
     nav.classList.toggle('show');
+
+    let bannerSection = document.getElementById('banner-section');
+    bannerSection.style.marginTop = nav.classList.contains('show') ? '110px' : '0';
 }
